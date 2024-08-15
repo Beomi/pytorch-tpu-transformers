87d6e260090dd37bd691d95a35427a736c767843
Junbum Lee <jun@beomi.net>
2024-08-15 23:30:26 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,14 @@ tssha() {
     gcloud compute tpus tpu-vm ssh "$1" --zone us-central2-b --worker=all --command "$2"
 }
 
+echo "[local] Killing TPU"
+tssha "sudo fuser -k /dev/accel0"
+
+echo "[local] Removing TPU Lock"
+tssha "sudo rm -f /tmp/libtpu_lockfile"
+
+echo "[local] Removing screens"
+tssha "killall screen"
+
 tssha v4-256 'screen -S trainer -X quit'
-tssha v4-256 'screen -dmS trainer bash -c "cd pytorch-tpu-transformers && git pull && ./train.sh"'
+tssha v4-256 'screen -dmSL trainer bash -c "cd pytorch-tpu-transformers && git pull && ./train.sh"'
