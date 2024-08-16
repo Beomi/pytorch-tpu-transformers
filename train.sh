cda2b40b7e3f88ec1715b3ff3d1f003633482a87
Junbum Lee <jun@beomi.net>
2024-08-16 16:29:25 +0900

    spmd_2d_sharding 4 == MP=4

--- train.sh
+++ train.sh
@@ -35,5 +35,5 @@ python examples/pytorch/language-modeling/run_clm.py \
     --dataloader_drop_last yes \
     --block_size 2048 \
     --preprocessing_num_workers 32 \
-    --spmd_2d_sharding 1 \
+    --spmd_2d_sharding 4 \
     --spmd_grad_chkpt
