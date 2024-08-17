e636e3597cafdee694cf41acdf2b243b8f58b6c9
Junbum Lee <jun@beomi.net>
2024-08-17 17:26:12 +0900

    add kill_tpu_docker.sh

---
+++ kill_tpu_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Find the container ID(s) for the image that starts with "tpuvm"
+container_ids=$(sudo docker ps --filter "ancestor=tpuvm" --format "{{.ID}}")
+
+# Check if any container IDs were found
+if [ -z "$container_ids" ]; then
+    echo "No running containers found with an image that starts with 'tpuvm'."
+else
+    # Kill each container found
+    for id in $container_ids; do
+        echo "Killing container with ID: $id"
+        sudo docker kill $id
+    done
+fi
