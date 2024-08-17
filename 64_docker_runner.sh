67b61483714c9a809b088136c3a87ace9f7317b4
Junbum Lee <jun@beomi.net>
2024-08-17 17:08:44 +0900

    Add 64_docker_runner.sh

---
+++ 64_docker_runner.sh
@@ -0,0 +1,24 @@
+DOCKER_CMD='python -u \
+    /transformers/examples/pytorch/language-modeling/run_clm.py \
+    --tokenizer_name beomi/Solar-Ko-Recovery-11B \
+    --model_name_or_path beomi/Solar-Ko-Recovery-11B \
+    --dataset_name maywell/korean_textbooks \
+    --dataset_config_name claude_evol \
+    --per_device_train_batch_size 256 \
+    --num_train_epochs 3 \
+    --do_train \
+    --output_dir /mnt/nfs_share/output_dir/beomi/Solar-Ko-Recovery-11B/ \
+    --overwrite_output_dir \
+    --save_strategy epoch \
+    --logging_strategy steps \
+    --logging_steps 1 \
+    --remove_unused_columns no \
+    --optim adafactor \
+    --torch_dtype bfloat16 \
+    --dataloader_drop_last yes \
+    --spmd_grad_chkpt \
+    --spmd_defer_init \
+'
+
+echo "sudo docker run --net=host --privileged -t -d tpuvm $DOCKER_CMD"
+# tssha v4-64 'sudo docker run --net=host --privileged -t -d tpuvm "$DOCKER_CMD"'
